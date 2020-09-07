7686da6d0dd01347641ef7515f40a202e69c826f
dpajek <dan.pajek@gmail.com>
2020-09-07 14:21:52 -0400

    search not buggy

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    futureArticles = fetchArticles();
+    futureArticles = fetchArticles('');
   }
 
   @override
@@ -86,48 +86,119 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Article>>(
-          future: futureArticles,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              //return Text(snapshot.data[0].title);
-              List<Article> arts = snapshot.data;
-
-              return ListView.builder(
-                  itemCount: arts.length,
-                  itemBuilder: (context, index) {
-                    Article art = arts[index];
-
-                    return Container(
-                      height: 60,
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(art.urlToImage),
-                        ),
-                        trailing: Text(art.author),
-                        title: new Text(art.title),
-                        onTap: () {
-                          //Navigator.push(context, new MaterialPageRoute(builder: (context) => new Home()));
-                        },
+      body: Column(
+        children: [
+          Container(
+            constraints: BoxConstraints(maxWidth: 400), //max width on search
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      //enabledBorder: UnderlineInputBorder(),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme.of(context).focusColor),
                       ),
-                    );
-                  });
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+                      hintText: 'Search',
+                      //contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                    ),
+                    onSubmitted: (searchTerm) {
+                      futureArticles = fetchArticles(searchTerm);
+                      //print('Here is a new term: $searchTerm');
+                      setState(() {
+                        futureArticles = fetchArticles(searchTerm);
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<Article>>(
+              future: futureArticles,
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    (snapshot.connectionState == ConnectionState.done)) {
+                  //return Text(snapshot.data[0].title);
+                  List<Article> arts = snapshot.data;
 
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
-        ),
-      
+/*
+                  print(arts.length);
+                  print(arts[2].title);
+                  print(arts[2].author);
+                  print(arts[14].urlToImage);
+                  print(snapshot.connectionState.toString());
+                  */
+
+                  return ListView.builder(
+                      itemCount: arts.length,
+                      itemBuilder: (context, index) {
+                        Article art = arts[index];
+
+                        //print(index);
+
+                        return Container(
+                          height: 60,
+                          child: ListTile(
+                            leading: art.urlToImage == null
+                                ? CircleAvatar(
+                                    backgroundColor: Colors.grey,
+                                  )
+                                : CircleAvatar(
+                                    backgroundImage:
+                                        NetworkImage(art.urlToImage),
+                                  ),
+                            trailing:
+                                Text(art.author == null ? '' : art.author),
+                            title: Text(art.title == null ? '' : art.title),
+                            onTap: () {
+                              //Navigator.push(context, new MaterialPageRoute(builder: (context) => new Home()));
+                            },
+                          ),
+                        );
+                      });
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                // By default, show a loading spinner.
+                return Center(
+                  child: SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-Future<List<Article>> fetchArticles() async {
-  final response = await http.get(
-      'https://newsapi.org/v2/everything?q=apples&apiKey=72d027b1dc974964b9af07784ff0636d');
+Future<List<Article>> fetchArticles(String searchTerm) async {
+  // if no search term, show headlines!
+  final response = searchTerm == ''
+      ? await http.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=72d027b1dc974964b9af07784ff0636d')
+      : await http.get('https://newsapi.org/v2/everything?q=' +
+          searchTerm +
+          '&apiKey=72d027b1dc974964b9af07784ff0636d');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -141,8 +212,8 @@ Future<List<Article>> fetchArticles() async {
     final numArticles =
         jResponse['totalResults'] < 20 ? jResponse['totalResults'] : 20;
 
-    for (var i=0; i<numArticles; i++) {
-          arts.add(Article.fromJson(jResponse['articles'][i]));
+    for (var i = 0; i < numArticles; i++) {
+      arts.add(Article.fromJson(jResponse['articles'][i]));
     }
 
     //print(numArticles);
