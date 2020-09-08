d39abea80382c1b813ac45e8f1e5ed0f2ce03152
dpajek <dan.pajek@gmail.com>
2020-09-07 21:37:59 -0400

    Got cards to work

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo - Tier 2',
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        //cardColor: Colors.blueGrey,
+        cardTheme: CardTheme(
+          color: Colors.blueGrey,
+          shadowColor: Colors.redAccent,
+          elevation: 4,
+        ),
+
+        focusColor: Colors.redAccent,
+
+        appBarTheme: AppBarTheme(
+          color: Colors.blueGrey,
+          shadowColor: Colors.redAccent,
+          //elevation: 2
+        ),
+
+        buttonColor: Colors.red[300],
+
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Dashboard'),
     );
   }
 }
@@ -52,19 +61,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   Future<List<Article>> futureArticles;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   void initState() {
@@ -86,110 +83,263 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            constraints: BoxConstraints(maxWidth: 400), //max width on search
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            height: 70,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      //enabledBorder: UnderlineInputBorder(),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Theme.of(context).focusColor),
-                      ),
-                      hintText: 'Search',
-                      //contentPadding: const EdgeInsets.symmetric(vertical: 0),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // Search bar
+            Container(
+              constraints: BoxConstraints(maxWidth: 400), //max width on search
+              padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+              height: 30,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                      size: 30,
                     ),
-                    onSubmitted: (searchTerm) {
-                      futureArticles = fetchArticles(searchTerm);
-                      //print('Here is a new term: $searchTerm');
-                      setState(() {
-                        futureArticles = fetchArticles(searchTerm);
-                      });
-                    },
                   ),
-                ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        //enabledBorder: UnderlineInputBorder(),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Theme.of(context).focusColor),
+                        ),
+                        hintText: 'Search',
+                        //contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                      ),
+                      onSubmitted: (searchTerm) {
+                        futureArticles = fetchArticles(searchTerm);
+                        //print('Here is a new term: $searchTerm');
+                        setState(() {
+                          futureArticles = fetchArticles(searchTerm);
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ), // Search bar end
+
+            // Article Cards Row
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                _buildArticlesTitleRow(context),
+
+                //_buildFutureList(futureArticles),
+                _buildFutureCards(futureArticles),
               ],
             ),
-          ),
-          Expanded(
-            child: FutureBuilder<List<Article>>(
-              future: futureArticles,
-              builder: (context, snapshot) {
-                if (snapshot.hasData &&
-                    (snapshot.connectionState == ConnectionState.done)) {
-                  //return Text(snapshot.data[0].title);
-                  List<Article> arts = snapshot.data;
 
-/*
-                  print(arts.length);
-                  print(arts[2].title);
-                  print(arts[2].author);
-                  print(arts[14].urlToImage);
-                  print(snapshot.connectionState.toString());
-                  */
+            //_buildArticleOfTheDayTitleRow(context),
 
-                  return ListView.builder(
-                      itemCount: arts.length,
-                      itemBuilder: (context, index) {
-                        Article art = arts[index];
+            //_buildFutureArticleOfTheDay(futureArticles),
 
-                        //print(index);
-
-                        return Container(
-                          height: 60,
-                          child: ListTile(
-                            leading: art.urlToImage == null
-                                ? CircleAvatar(
-                                    backgroundColor: Colors.grey,
-                                  )
-                                : CircleAvatar(
-                                    backgroundImage:
-                                        NetworkImage(art.urlToImage),
-                                  ),
-                            trailing:
-                                Text(art.author == null ? '' : art.author),
-                            title: Text(art.title == null ? '' : art.title),
-                            onTap: () {
-                              //Navigator.push(context, new MaterialPageRoute(builder: (context) => new Home()));
-                            },
-                          ),
-                        );
-                      });
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-
-                // By default, show a loading spinner.
-                return Center(
-                  child: SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+
+Widget _buildArticlesTitleRow(context) => Container(
+                  constraints:
+                      BoxConstraints(maxWidth: 600), //max width on card titles
+                  padding: EdgeInsets.fromLTRB(10, 20, 10,
+                      0), //changed to top=20 after making scrollable
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Articles',
+                        //textAlign: TextAlign.left,
+                      ),
+                      FlatButton(
+                        //color: Colors.blue,
+                        textColor: Theme.of(context).buttonColor,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        padding: EdgeInsets.all(0),
+                        splashColor: Colors.blueAccent,
+                        onPressed: /*_pushAllArticles*/ () {},
+                        child: Text(
+                          "All Articles",
+                        ),
+                      )
+                    ],
+                  ),
+                );
+
+Widget _buildFutureCards(Future<List<Article>> futureArticles) =>
+    FutureBuilder<List<Article>>(
+      future: futureArticles,
+      builder: (context, snapshot) {
+        if (snapshot.hasData &&
+            (snapshot.connectionState == ConnectionState.done)) {
+          List<Article> arts = snapshot.data;
+
+          return Container(
+            height: 180,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: arts.length,
+                itemBuilder: (context, index) {
+                  Article art = arts[index];
+
+                  return _buildArticleCard(art);
+                }),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        // By default, show a loading spinner.
+        return Center(
+          child: SizedBox(
+            width: 30,
+            height: 30,
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+
+// Card Widget
+Widget _buildArticleCard(Article art) => Container(
+      //Expanded(
+      //child: SizedBox(
+      //height: 180,
+      width: 160,
+      child: InkWell(
+        onTap: () {
+          /*
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text(art.title),
+                    ),
+                    body: _buildDetailsPage(art),
+                  );
+                },
+              ),
+            );
+            */
+        },
+        child: Card(
+          // This ensures that the Card's children (including the ink splash) are clipped correctly.
+          clipBehavior: Clip.antiAlias,
+          //color: Colors.blueGrey,
+          //shape: null,
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: art.urlToImage == null
+                    ? Icon(Icons.error_outline, size: 75)
+                    : Image(
+                        image: NetworkImage(art.urlToImage),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter,
+                        width: double.infinity,
+                      ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      art.title == null
+                          ? 'no title'
+                          : (art.title.length > 40
+                              ? art.title.substring(0, 40)
+                              : art.title),
+                      style: TextStyle(fontSize: 13.0, color: Colors.white),
+                      textAlign: TextAlign.left,
+                    ),
+                    Text(
+                      art.author == null
+                          ? 'no author'
+                          : (art.author.length > 40
+                              ? art.author.substring(0, 40)
+                              : art.author),
+                      style: TextStyle(fontSize: 11.0, color: Colors.white),
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+// Keep this for future reference
+/*
+Widget _buildFutureList(Future<List<Article>> futureArticles) => Expanded(
+      child: FutureBuilder<List<Article>>(
+        future: futureArticles,
+        builder: (context, snapshot) {
+          if (snapshot.hasData &&
+              (snapshot.connectionState == ConnectionState.done)) {
+            List<Article> arts = snapshot.data;
+
+            return ListView.builder(
+                itemCount: arts.length,
+                itemBuilder: (context, index) {
+                  Article art = arts[index];
+
+                  return Container(
+                    height: 60,
+                    child: ListTile(
+                      leading: art.urlToImage == null
+                          ? CircleAvatar(
+                              backgroundColor: Colors.grey,
+                            )
+                          : CircleAvatar(
+                              backgroundImage: NetworkImage(art.urlToImage),
+                            ),
+                      trailing: Text(art.author == null ? '' : art.author),
+                      title: Text(art.title == null ? '' : art.title),
+                      onTap: () {
+                        //Navigator.push(context, new MaterialPageRoute(builder: (context) => new Home()));
+                      },
+                    ),
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          // By default, show a loading spinner.
+          return Center(
+            child: SizedBox(
+              width: 30,
+              height: 30,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
+    );
+    */
 
 Future<List<Article>> fetchArticles(String searchTerm) async {
   // if no search term, show headlines!
